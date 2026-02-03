6ab220940098b177a318f417eab71320666b9b94
haymcarthur <haylee.mcarthur@familysearch.org>
2026-02-03 13:37:15 -0700

    Change port from 3002 to 3004 for port reorganization

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3002
+    port: 3004,
+    open: false,
+    strictPort: true
+  },
+  preview: {
+    port: 3004
   }
 })
